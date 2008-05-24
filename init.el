83ba9dbf808a9abc344c378bf0a3e3305b8ede15
Sigurd Meldgaard <Sigurd Meldgaard>
2008-05-24 14:48:05 +0200

    Initial git-import

---
+++ init.el
@@ -0,0 +1,7 @@
+(load "loadfiles")
+(load "settings")
+(load "small")
+(load "keybindings")
+(setq custom-file (concat emacsmoduler-path "/custom.el"))
+  
+(load custom-file)
---
+++ settings.el
@@ -0,0 +1,92 @@
+(when (featurep 'mule)
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'latin-1)
+  (set-language-environment 'utf-8)
+  (server-start))
+
+(setq org-log-done t)
+(setq org-export-with-LaTeX-fragments t)
+
+;(defvar LaTex-mode-hook)
+(eval-after-load 'latex '(add-to-list 'LaTeX-mode-hook (lambda () (flyspell-mode 1))))
+
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(setq inhibit-startup-message 1)
+(abbrev-mode 1)
+
+;;;Use of time
+(display-time)
+(add-hook 'diary-hook 'appt-make-list)
+(set-variable 'timeclock-modeline-display t)
+
+(add-hook 'makefile-mode-hook (lambda () (setq indent-tabs-mode t)))
+
+(defconst use-backup-dir t)
+(setq backup-directory-alist (quote ((".*" . "~/backup/temp/")))
+      version-control t                ; Use version numbers for backups
+      kept-new-versions 5             ; Number of newest versions to keep
+      kept-old-versions 2              ; Number of oldest versions to keep
+      delete-old-versions t            ; Ask to delete excess backup versions?
+      backup-by-copying-when-linked t) ; Copy linked files, don't rename.
+
+(require 'font-lock)
+;(setq font-lock-support-mode 'lazy-lock-mode)
+(setq font-lock-maximum-decoration t)
+(global-font-lock-mode t)
+
+(try "mmm-php" (mmm-add-mode-ext-class 'html-mode "\\.php\\'" 'html-php))
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.php[34]?\\'" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.htm[l]?\\'" . nxml-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . css-mode))
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.d\\'" . d-mode))
+
+(setq tramp-default-method "scp")
+
+(better-registers-install-save-registers-hook)
+(setq better-registers-save-file "~/.registers.el")
+(if (file-exists-p better-registers-save-file)
+    (load better-registers-save-file))
+
+;(when (eq window-system 'x)
+;  (set-face-font 'default "-*-fixed-medium-r-*-*-12-*-*-*-*-*-iso8859-*");
+;  ;; In "fontset-standard" use "misc fixed" for the charset
+;  ;; `mule-unicode-0100-24ff'
+;  (set-fontset-font "fontset-standard"
+;                    'mule-unicode-0100-24ff
+;                   "-*-fixed-medium-r-*-*-12-*-*-*-*-*-iso10646-1")
+;  (shell-command
+;   (concat "xmodmap "
+;          "-e 'keycode 115 = Hyper_R' "
+;           "-e 'add mod4 = Hyper_R' ")))
+
+
+;;Firefox as standard browser
+(setq browse-url-generic-program "firefox")
+
+(defun yes-or-no-p (prompt)
+  "redirects to y-or-n-p"
+  (y-or-n-p prompt))
+
+(when (featurep 'x)
+  (setq x-select-enable-clipboard t)
+  (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+  (setf x-select-enable-clipboard t))
+
+(set-variable 'visible-bell t)
+
+(when (or (functionp 'cua-mode)(featurep 'cua))
+  (cua-mode 1)
+  (setq
+   cua-auto-mark-last-change t
+   cua-enable-cua-keys t
+   cua-enable-register-prefix (quote not-ctrl-u)
+   cua-highlight-region-shift-only nil
+   cua-keep-region-after-copy nil))
+
+(my-color-theme)
