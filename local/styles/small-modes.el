eda18049997e0f6d6898fb79836df0a5ae2e72ed
stm <sigurdmeldgaard@sigurd-meldgaards-macbook-2.local>
2008-12-12 15:08:39 +0100

    Autoload of speck mode

--- local/styles/small-modes.el
+++ local/styles/small-modes.el
@@ -4,6 +4,7 @@
 (autoload 'haml-mode "haml-mode" nil t)
 (autoload 'erlang-mode "erlang" nil t)
 (autoload 'peep-mode "peep" nil t)
+(autoload 'speck-mode "speck" nil t)
 (autoload 'russian-mode "russian-mode" nil t)
 (load "small")
 
