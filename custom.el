b0ed4ac40f82ec664fa8aaa873d79c7b70531f70
stm <sigurdmeldgaard@stmtop.local>
2009-02-03 08:42:47 +0100

    * custom.el: Added sml-mode

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(agda2-fontset-name nil))
+ '(agda2-fontset-name nil)
+ '(double-map (quote ((59 ";" "æ") (39 "ø" "'") (91 "[" "å") (58 ":" "Æ") (34 "\"" "Ø") (123 "{" "Å"))))
+ '(tex-start-commands "--shell-escape \"\\nonstopmode\\input\""))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
