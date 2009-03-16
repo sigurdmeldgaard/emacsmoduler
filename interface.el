9eb35f4784ee9f75ce77d2507215fae0ee17d476
stm <sigurdmeldgaard@stmtop.local>
2009-03-16 19:26:12 +0100

    Incr at point

--- interface.el
+++ interface.el
@@ -35,7 +35,6 @@
 
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
-(menu-bar-mode -1)
 (setq inhibit-startup-message 1)
 (abbrev-mode 1)
 
@@ -79,6 +78,7 @@
 (require 'vc-ediff)
 (require 'find-cmd)
 (require 'project-root)
+(require 'mindent)
 
 (setq custom-file (concat emacsmoduler-path "/custom.el"))
 (load custom-file)
@@ -101,3 +101,34 @@
 (define-key isearch-mode-map (kbd "C-M-o") 'isearch-occur)
 
 (require 'incr-at-point)
+
+;; (defadvice kill-buffer (around my-kill-buffer-check activate)
+;;   "Prompt when a buffer is about to be killed."
+;;   (let* ((buffer-file-name (buffer-file-name))
+;;          backup-file)
+;;     ;; see 'backup-buffer
+;;     (if (and (buffer-modified-p)
+;;              buffer-file-name
+;;              (file-exists-p buffer-file-name)
+;;              (setq backup-file (car (find-backup-file-name buffer-file-name))))
+;;         (let ((answer (completing-read (format "Buffer modified %s, (d)iff, (s)ave, (k)ill? " (buffer-name))
+;;                                        '("d" "s" "k") nil t)))
+;;           (cond ((equal answer "d")
+;;                  (set-buffer-modified-p nil)
+;;                  (let ((orig-buffer (current-buffer))
+;;                        (file-to-diff (if (file-newer-than-file-p buffer-file-name backup-file)
+;;                                          buffer-file-name
+;;                                        backup-file)))
+;;                    (set-buffer (get-buffer-create (format "%s last-revision" (file-name-nondirectory file-to-diff))))
+;;                    (buffer-disable-undo)
+;;                    (insert-file-contents file-to-diff nil nil nil t)
+;;                    (set-buffer-modified-p nil)
+;;                    (setq buffer-read-only t)
+;;                    (ediff-buffers (current-buffer) orig-buffer)))
+;;                 ((equal answer "k")
+;;                  (set-buffer-modified-p nil)
+;;                  ad-do-it)
+;;                 (t
+;;                  (save-buffer)
+;;                  ad-do-it)))
+;;       ad-do-it)))
