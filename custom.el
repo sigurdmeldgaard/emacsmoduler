ff8d04a79ef14ad575d4e8da3f5f3a3ff03c589e
Sigurd Torkel Meldgaard <stm@camel00.daimi.au.dk>
2010-01-15 23:51:17 +0100

    Lots of nice customizations

--- custom.el
+++ custom.el
@@ -3,25 +3,33 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
-'(browse-url-browser-function (quote browse-url-generic))
- '(dired-recursive-copies (quote always))
- '(dired-recursive-deletes (quote top))
+ '(browse-url-browser-function (quote browse-url-generic))
+ '(dired-recursive-copies (quote always) t)
+ '(dired-recursive-deletes (quote top) t)
  '(global-hl-line-mode nil nil (hl-line))
  '(haskell-program-name "ghci \"+.\"")
  '(haskell-refac-chasePaths (quote ("/home/sigurd/haskellleg" "/home/sigurd/haskellleg/HaRe_20012006/tools/base/tests/HaskellLibraries" "/usr/lib/ghc-6.4.1")))
  '(indent-tabs-mode nil)
- '(ispell-program-name "aspell")
- '(nxml-slash-auto-complete-flag t)
+ '(isa-isatool-command "~/bin/isabelle")
+ '(isar-shortcut-alist (quote (("|}" . "⦄") ("{|" . "⦃") ("/\\\\" . "⋀") ("``" . "´") ("\\/" . "∨") ("/\\" . "∧") ("+O" . "⊕") ("-O" . "⊖") ("xO" . "⊗") ("/O" . "⊘") (".O" . "⊙") ("|+" . "†") ("|++" . "‡") ("<=" . "≤") ("|-" . "⊢") (">=" . "≥") ("-|" . "⊣") ("||" . "∥") ("==" . "≡") ("~=" . "≠") ("~:" . "∉") ("~~~" . "≍") ("~~" . "≈") ("~==" . "≅") ("|<>|" . "⋈") ("|=" . "⊨") ("=." . "≐") ("_|_" . "⊥") ("</" . "≮") (">=/" . "≱") ("=/" . "≠") ("==/" . "≢") ("~/" . "≁") ("~=/" . "≄") ("~~/" . "≉") ("~==/" . "≇") ("<-" . "←") ("->" . "→") ("=>" . "⇒") ("<->" . "↔") ("<=>" . "⇔") ("|->" . "↦") ("<--" . "⟵") ("<==" . "⟸") ("-->" . "⟶") ("==>" . "⟹") ("<==>" . "⟷") ("|-->" . "⟼") ("<-->" . "⟷") ("<<" . "«") ("[|" . "⟦") (">>" . "»") ("|]" . "⟧") ("---" . "—") ("\\nat" . "ℕ") ("\\int" . "ℤ") ("\\rat" . "ℚ") ("\\real" . "ℝ") ("\\complex" . "ℂ") ("\\euro" . "€") ("\\yen" . "¥") ("\\cent" . "¢"))))
+ '(isar-unicode-tokens-enable t)
+ '(isar-x-symbol-enable nil)
+ '(ispell-program-name "aspell" t)
+ '(lpr-command "kprinter")
+ '(nxml-slash-auto-complete-flag t t)
  '(org-agenda-files (quote ("~/izmir/todo.org")))
+ '(python-python-command "ipython" t)
  '(safe-local-variable-values (quote ((TeX-master . rapport) (TeX-master . "rapport"))))
- '(scheme-program-name "mzscheme")
+ '(scheme-program-name "guile")
+ '(server-temp-file-regexp "^/tmp/Re\\|/draft$\\|itsalltext/")
  '(smiley-regexp-alist nil)
- '(speedbar-directory-unshown-regexp "^\\(CVS\\|RCS\\|SCCS\\|_DARCS\\|\\..*\\)\\'")
- '(speedbar-use-images nil)
+ '(speedbar-directory-unshown-regexp "^\\(CVS\\|RCS\\|SCCS\\|_DARCS\\|\\..*\\)\\'" t)
+ '(speedbar-use-images nil t)
  '(spell-command "aspell"))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(flymake-errline ((((class color)) (:background "DarkRed"))))
+ '(flymake-warnline ((((class color)) (:background "DarkBlue")))))
