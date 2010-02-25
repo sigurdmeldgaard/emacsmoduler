cfc04da114494ed54c0f47e60162593849c202c9
Sigurd Meldgaard <stm@cs.au.dk>
2010-02-25 10:58:17 +0100

    Added maxframe when loading on osx

--- darwin.el
+++ darwin.el
@@ -1,5 +1,6 @@
 ;; Standard browser.
 (setq browse-url-generic-program "open")
 
+(robust-load-elisp (load (concat grail-dist-elisp "maxframe")))
 
 (robust-load-elisp (load (concat grail-dist-elisp "growl")))
