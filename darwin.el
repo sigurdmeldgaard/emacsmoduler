536fc68e53509845827d7458f15a3d10ca66cc8f
Sigurd Meldgaard <stm@cs.au.dk>
2010-03-08 13:03:40 +0100

    Use built-in viewer for latex-pdfs on mac-os

--- darwin.el
+++ darwin.el
@@ -1,6 +1,12 @@
 ;; Standard browser.
 (setq browse-url-generic-program "open")
 
+;; Use built-in viewer for pdf's
+(eval-after-load 'latex 
+  '(progn
+     (add-to-list 'TeX-output-view-style
+                  '("^pdf$" "." "open %o %(outpage)"))))
+
 (robust-load-elisp (load (concat grail-dist-elisp "maxframe")))
 
 (robust-load-elisp (load (concat grail-dist-elisp "growl")))
