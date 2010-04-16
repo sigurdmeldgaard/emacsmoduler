4fbec8338d3cc99ea13f4d3b35a446c7ce118d48
Sigurd Torkel Meldgaard <stm@aravis.daimi.au.dk>
2010-04-16 14:22:14 +0200

    Made the bell silent
    (How come it was not?)

--- gui.el
+++ gui.el
@@ -1,12 +1,6 @@
 (robust-load-elisp (load (concat grail-dist-elisp "color-theme")))
 (robust-load-elisp (load (concat grail-local-elisp "my-color-theme")))
-(color-theme-initialize)
 
-(custom-set-faces
- '(diff-added ((t (:foreground "Green"))) 'now)
- '(diff-removed ((t (:foreground "Red"))) 'now))
-
-(fset 'original-color-theme (color-theme-make-snapshot))
 (my-color-theme)
 
 (add-hook 'after-make-frame-functions
@@ -26,8 +20,7 @@
  '(flymake-errline ((t (:background "DarkRed"))))
  '(flymake-warnline ((((class color)) (:background "DarkBlue")))))
 
-
-(set-variable 'visible-bell nil)
+(set-variable 'visible-bell t)
 
 (defvar fullscreen-p nil)
 (defun toggle-fullscreen()
