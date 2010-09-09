85b1b0f54ab380a685bbbc36c5d0b1e0a8f5eefc
Sigurd Meldgaard <stm@cs.au.dk>
2010-09-09 12:39:49 +0200

    New command: unfill paragraph

--- commands.el
+++ commands.el
@@ -16,6 +16,14 @@
 (require 'smex)
 (eval-after-load 'grail '(progn (message "got smex") (smex-initialize)))
 
+;;; Stefan Monnier <foo at acm.org>.
+;;; From: http://www.emacswiki.org/emacs/UnfillParagraph
+(defun unfill-paragraph ()
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
+
 (when (or (functionp 'cua-mode) (featurep 'cua))
   (cua-mode 1)
   (setq
