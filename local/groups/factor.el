df9fb2c493b0bf7a71b76f357dd2f6ffdd30d1df
Sigurd Meldgaard <stm@cs.au.dk>
2010-09-16 15:31:23 +0200

    Turned factor loading into an autoload

--- local/groups/factor.el
+++ local/groups/factor.el
@@ -1,3 +1,5 @@
-(load-file "~/leg/factor/misc/fuel/fu.el")
+(autoload 'factor-mode "~/leg/factor/misc/fuel/fu.el"
+  "Major mode for editing Factor source." t)
+
 (setq fuel-listener-factor-binary "~/leg/factor/factor")
 (setq fuel-listener-factor-image "~/leg/factor/factor.image")
