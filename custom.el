0851c37ae2c7d0fd18f49397e0a154f871aef3b6
Sigurd Meldgaard <stm@cs.au.dk>
2010-10-04 13:58:27 +0200

    Mixed customizations

--- custom.el
+++ custom.el
@@ -1,12 +1,12 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(LaTeX-command "latex --shell-escape")
  '(browse-url-browser-function (quote browse-url-generic))
- '(dired-recursive-copies (quote always) t)
- '(dired-recursive-deletes (quote top) t)
+ '(dired-recursive-copies (quote always))
+ '(dired-recursive-deletes (quote top))
  '(global-hl-line-mode nil nil (hl-line))
  '(haskell-program-name "ghci")
  '(haskell-refac-chasePaths (quote ("/home/sigurd/haskellleg" "/home/sigurd/haskellleg/HaRe_20012006/tools/base/tests/HaskellLibraries" "/usr/lib/ghc-6.4.1")))
@@ -18,11 +18,12 @@
  '(ispell-program-name "aspell" t)
  '(load-home-init-file t t)
  '(lpr-command "kprinter")
- '(nxml-slash-auto-complete-flag t t)
+ '(nxml-slash-auto-complete-flag t)
  '(org-agenda-files nil)
- '(python-python-command "ipython")
+ '(python-python-command "python")
  '(safe-local-variable-values (quote ((TeX-master . rapport) (TeX-master . "rapport"))))
  '(scheme-program-name "guile")
+ '(semantic-c-dependency-system-include-path (quote ("/usr/include" "/opt/local/include")))
  '(server-temp-file-regexp "^/tmp/Re\\|/draft$\\|itsalltext/")
  '(smiley-regexp-alist nil)
  '(speck-save-ask t)
@@ -31,10 +32,10 @@
  '(spell-command "aspell")
  '(user-mail-address "stm@cs.au.dk"))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(diff-added ((t (:foreground "Green"))))
  '(diff-removed ((t (:foreground "Red"))))
  '(flymake-errline ((((class color)) (:background "DarkRed"))))
