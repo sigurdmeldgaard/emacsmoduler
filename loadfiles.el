1a7bd991d83a6c9a560adf38bf2d5cb4a6159037
Sigurd Meldgaard <post@sigurdmeldgaard.dk>
2012-04-17 13:51:39 +0300

    Removed haskell mode

--- loadfiles.el
+++ loadfiles.el
@@ -67,28 +67,6 @@
 (autoload 'gnuplot-make-buffer "gnuplot" "open a buffer in gnuplot mode" t)
 
 
-;;; haskell
-
-(load (concat dist-elisp "haskell-mode-2.3/haskell-site-file.el"))
-
-(add-hook 'haskell-mode-hook 'my-mmm-mode)
-
-(mmm-add-classes
- '((literate-haskell-bird
-    :submode text-mode
-    :front "^[^>]"
-    :include-front true
-    :back "^>\\|$"
-    )
-   (literate-haskell-latex
-    :submode literate-haskell-mode
-    :front "^\\\\begin{code}"
-    :front-offset (end-of-line 1)
-    :back "^\\\\end{code}"
-    :include-back nil
-    :back-offset (beginning-of-line -1)
-    )))
-
 (defun my-mmm-mode ()
   ;; go into mmm minor mode when class is given
   (make-local-variable 'mmm-global-mode)
